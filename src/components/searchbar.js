8270b8c005dffb76f1b1360cbeb5a6aa6f87e2ce
Jack11709 <jackmay89@live.com>
2017-09-27 15:54:30 +0100

    commit

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 
+// state is a plain javascript object that exists on any class based component
 class SearchBar extends Component {
-  render() {
-    return <input onChange={this.onInputChange} />
+  // constructor method to initialize state. must be on a class component not functional, always first function called
+  constructor(props){
+    // calling the parent method of class component
+    super(props);
+
+    // initializing state by assiging it to a plain object using this.state, only place we construct state in this way
+    this.state = { term: '' };
   }
 
-  onInputChange(e) {
-    console.log(e.target.value);
+
+  render() {
+    // using setState method here to record the value of the input to the state
+    return (
+      <div className="search-bar">
+        <input
+        // by setting this inputs value to state.term its being made into a controlled component, its value is set by state. when the setState method is called it is re-rendering this component, at which point the value is then updated.
+        value = { this.state.term }
+        onChange={e => this.onInputChange(e.target.value)} />
+      </div>
+    );
   }
+
+  onInputChange(term){
+    this.setState({term});
+    this.props.onSearchTermChange(term);
+  }
+
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,53 @@
 // Create a new component, this will produce some html.
 // Take this components generated html and put it on the DOM
-import React from 'react';
-import ReactDOM from 'react-dom'
-import SearchBar from './components/searchbar'
-
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTsearch from 'youtube-api-search';
+import SearchBar from './components/searchbar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyC4isWVq58rvw56p7RZGherMZS2ZUOVA2g';
 
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('React JS');
+  }
+
+  videoSearch(term){
+    YTsearch({key: API_KEY, term: term},(videos) => {
+      // as using es6 and using same variable name as the key from state, can just write it once.
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
+    return (
+      <div>
+        <h1 className="title">Jack<span>Tube</span></h1>
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
